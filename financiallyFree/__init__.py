b17ab2a256173b23269bdb95cc4d00006de0d3f3
Mike1938 <mmaldonadosan1938@gmail.com>
2021-10-01 17:14:25 -0400

    imported blueprint and render_template to start working on views.py and create the first route

---
+++ financiallyFree/__init__.py
@@ -0,0 +1,20 @@
+import os
+import dotenv
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = os.getenv('key'),
+        DATABASE = os.path.join(app.instance_path, 'flask.sqlite'),
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    # * Import views routes
+    from .views import views
+    app.register_blueprint(views, url_prefix ='/')
+    
+    return app
