2206d02e94acc585fcfc48c9d6167267ccaaaaa2
Mike1938 <mmaldonadosan1938@gmail.com>
2021-10-01 23:21:47 -0400

    Added styling to buttons, adjusted styles, added 2 p tags for the form validation, made form validation for expensesform

--- financiallyFree/static/js/landing.js
+++ financiallyFree/static/js/landing.js
@@ -1,6 +1,8 @@
 const expenseTitle = document.getElementById('expenseTitle');
 const expenseCost = document.getElementById('expenseCost');
 const expenseForm = document.getElementById('expenseForm');
+const totalExpenses = document.querySelector('#totalExpenses')
+const errorChecking = document.querySelectorAll('.errorChecking')
 
 // * Function in charge of creating rgb random colors
 const ranColor = ()=>{
@@ -21,6 +23,7 @@ let backgroundColor = [
     'rgb(153, 102, 255)',
     'rgb(255, 159, 64)'
 ]
+
 // * Function in charge of creating pie chart
 const createChart = ()=>{
     let result = new Chart(expensePi, {
@@ -43,16 +46,53 @@ const createChart = ()=>{
     return result
 }
 
+// *Function capable of doin the sum of all the expenses
+const sumExpenses = (data)=>{
+    let sum = 0;
+    data.forEach((d)=>{
+        sum += d;
+    })
+    return `$${sum}`
+}
+
+totalExpenses.textContent = sumExpenses(data);
+
+
 let expensePi = document.getElementById('expensePi');
 let myChart = createChart()
 // *Event listener that is waiting on user to hit confirm, data user input data is then added to the chart dynamically
 expenseForm.addEventListener('submit',(e)=>{
     e.preventDefault();
-    data.push(Number(expenseCost.value))
-    labels.push(expenseTitle.value)
-    backgroundColor.push(ranColor())
-    myChart.destroy()
-    myChart = createChart()
+    // *Verification of user inputs   
+    let title = expenseTitle.value.trim()
+    let cost = Number(expenseCost.value)
+    if(title === ""){
+        errorChecking[0].classList.add('error')
+        errorChecking[0].textContent = "Title cannot be left empty"
+    }
+    if(isNaN(cost)){
+        errorChecking[1].classList.add('error')
+        errorChecking[1].textContent = "Cannot be text, must be number"
+    }
+    else if(cost <= 0){
+        errorChecking[1].classList.add('error')
+        errorChecking[1].textContent = "Cannot less or equal to 0"
+    }
+    // * After verification of input if both are true then all data is push to chart
+    if(title && cost){
+        if(errorChecking[0].textContent || errorChecking[1].textContent){
+            errorChecking[0].classList.remove('error')
+            errorChecking[1].classList.remove('error')
+        }
+        data.push(Number(expenseCost.value))
+        labels.push(title)
+        backgroundColor.push(ranColor())
+        myChart.destroy()
+        myChart = createChart()
+        totalExpenses.textContent = sumExpenses(data);
+    }
+    expenseCost.value = ""
+    expenseTitle.value = ""
 });
 
 
